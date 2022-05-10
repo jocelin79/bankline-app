0ca427d608d74bc5cf27e13634f3766720bca194
Jocelin Nunes <jocelinnunes@gmail.com>
2022-05-10 17:40:51 -0300

    atualização do baseUrl

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://git.heroku.com/st-bankline-api.git';
 
 @Injectable({
   providedIn: 'root'
